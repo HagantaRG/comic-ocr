4f6f837ab3f6823becd0450f6b2dbf906c2dbe55
HagantaRG <50399914+haganta-g@users.noreply.github.com>
2026-07-29 01:05:09 +0700

    Modified testing script.

--- src/test.py
+++ src/test.py
@@ -3,15 +3,16 @@ from pathlib import Path
 
 from html_utils import Page, make_html_file
 
-file_list: list[str] = os.listdir("test-pics/temporary")
+folder_path: str = "C:\\Users\\rhaga\\Documents\\test"
+file_list: list[str] = os.listdir(folder_path)
 file_list.sort(key=lambda fname: int(fname.split('.')[0]))
 page_num = 0
 page_batch: int = 5
 batches: list = [[]]
 manga: dict[int, Page] = {}
-processing_files: list[str] = file_list[2:16]
+processing_files: list[str] = file_list
 for num, file in enumerate(processing_files):
-    path = Path(f"test-pics/temporary/{file}")
+    path = Path(f"{folder_path}/{file}")
     manga[num + 1] = Page(
         img_filepath=path,
         page_num=num + 1,
@@ -19,5 +20,5 @@ for num, file in enumerate(processing_files):
     )
 
 final_html: str = make_html_file(manga)
-with open("./HELP.html", "w", encoding="utf-8") as f:
+with open(f"{folder_path}/HELP.html", "w+", encoding="utf-8") as f:
     f.write(final_html)
